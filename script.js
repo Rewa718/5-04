421cb4476c23af2602636be0b45854df77289231
Rewa Patil <>
2021-05-03 17:21:01 +0000

    format

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 document.getElementById('change-image')
-  .addEventListener('click', image)
+.addEventListener('click', image)
 document.getElementById('change-text')
-  .addEventListener('click', text)
+.addEventListener('click', text)
 document.getElementById('hide')
-  .addEventListener('click', hide)
+.addEventListener('click', hide)
 document.getElementById('show')
-  .addEventListener('click', show)
+.addEventListener('click', show)
 document.getElementById('bg')
-  .addEventListener('click', background)
+.addEventListener('click', background)
 
 function image () {
-  document.getElementById('picture').src = 'images/dog.jpeg'
+document.getElementById('picture').src = 'images/dog.jpeg'
 }
 
 function text () {
-  document.getElementById('text').innerHTML = 'Dogs are the most common pets'
+document.getElementById('text').innerHTML = 'Dogs are the most common pets'
 }
 
 function hide () {
-  document.getElementById('text').style.display='none'
+document.getElementById('text').style.display = 'none'
 }
 
 function show () {
-  document.getElementById('text').style.display = 'inline'
+document.getElementById('text').style.display = 'inline'
 }
 
 function background () {
-  document.body.style.backgroundColor = 'grey'
+document.body.style.backgroundColor = 'grey'
 }
